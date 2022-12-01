8ad934dd8c0a6a685420f792c772495cbef10dd0
Michele Bucelli <michele.bucelli@polimi.it>
2022-12-01 11:44:23 +0000

    Upload lab 06

---
+++ lab-06/src/lab-06-exercise1.cpp
@@ -0,0 +1,25 @@
+#include <deal.II/base/convergence_table.h>
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "NonLinearDiffusion.hpp"
+
+// Main function.
+int
+main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
+
+  const unsigned int N      = 19;
+  const unsigned int degree = 1;
+
+  NonLinearDiffusion problem(N, degree);
+
+  problem.setup();
+  problem.solve_newton();
+  problem.output();
+
+  return 0;
+}
